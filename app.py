891f74193e9f65880cbe26269f56d72483d73527
Leonard Torres <adndeldato@gmail.com>
2026-04-23 23:33:34 -0500

    Mi primer commit: agregando app y workflow

---
+++ app.py
@@ -0,0 +1,5 @@
+def suma(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(f"La suma de 2 + 3 es: {suma(2, 3)}")
---
+++ test.py
@@ -0,0 +1,9 @@
+from app import suma
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+    print("funcion terminada")
+
+
+test_suma()
