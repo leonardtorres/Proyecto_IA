0ea1e980a5e8cb2142234fd1fb2047bc1eb3e959
Leonard Torres <adndeldato@gmail.com>
2026-04-23 23:52:15 -0500

    agregando app y workflow 3

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from app import suma
 def test_suma():
     assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
-    print("funcion terminada")
+    #print("funcion terminada")
 
 
-test_suma()
+#test_suma()
